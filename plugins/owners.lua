b3ea0e669604881eefe99eaf84997bbcab7f715e
spartacusteam <20kyg951k6@kismail.ru>
2016-07-10 09:16:15 -0700

    Update owners.lua

--- plugins/owners.lua
+++ plugins/owners.lua
@@ -1,3 +1,4 @@
+--start
 local function lock_group_namemod(msg, data, target)
   local group_name_set = data[tostring(target)]['settings']['set_name']
   local group_name_lock = data[tostring(target)]['settings']['lock_name']
