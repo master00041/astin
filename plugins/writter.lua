353fadd82da4e50afb67139c4cf48c84597886a0
master00041 <master041amin@gmail.com>
2016-07-31 12:27:45 +0430

    Update writter.lua

--- plugins/writter.lua
+++ plugins/writter.lua
@@ -186,7 +186,7 @@ local function run(msg, matches)
 		a=a+1
 		result_text = result_text..a.."- "..result[a].."\n\n"
 	end
-	return result_text.."______________________________\n📡Channel : @pedaret"
+	return result_text.."______________________________\n📡sudo : @pedaret"
 end
 
 return {
